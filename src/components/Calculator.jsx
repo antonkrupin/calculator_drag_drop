7227667ca5cf0013a72dd8c718c86d6fc5ae9083
antonkrupin <krupin_anton@mail.ru>
2023-03-10 13:50:58 +0300

    add ResultButton component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import ResultButton from './calc_buttons/ResultButton';
 import OperationsBlock from './OperationsBlock';
 import NumBlock from './NumBlock';
 import EqualButton from './calc_buttons/EqualButton';
@@ -9,6 +10,7 @@ import './Calculator.css';
 const Calculator = () => {
 	return (
 		<div className="calculator">
+			<ResultButton />
 			<OperationsBlock />
 			<NumBlock />
 			<EqualButton />
---
+++ src/components/calc_buttons/ResultButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ResultButton.css';
+
+const ResultButton = () => {
+	return (
+		<div className="resultButton">
+			<div className="button">
+				0
+			</div>   
+		</div>
+	)
+};
+
+export default ResultButton;
